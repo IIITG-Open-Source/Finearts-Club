a94339d3a626945c84f6dc383c5ade46d338c0a4
manan2110 <manan.co.in@gmail.com>
2021-06-03 04:19:59 +0530

    Added Posts (objects of model Post)

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all().order_by('-id')[:6]
+    coordinator = Coordinator.objects.all().order_by('-id')[:6]
+    return render(request, 'index.html', {'posts': posts})
