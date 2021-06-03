81ec3617b795ad5fc10f3fdd48a0fbd8e260f236
manan2110 <manan.co.in@gmail.com>
2021-06-03 05:54:33 +0530

    Backend : added backend to send email (for Contact us)

--- Alankrit/settings.py
+++ Alankrit/settings.py
@@ -122,5 +122,15 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+# Setting for sending Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOSTNAME = ''
+# EMAIL_HOST_PASSWORD = config('password')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 from .models import *
 # Create your views here.
 
@@ -6,4 +7,12 @@ from .models import *
 def index(request):
     posts = Post.objects.all().order_by('-id')[:6]
     coordinators = Coordinator.objects.all().order_by('-id')[:6]
+    if request.method == "POST":
+        subject = request.POST['subject']
+        name = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+        if name != "" and email != "" and subject != "":
+            send_mail(subject, message, email, [
+                      'alankrit@gmail.com'], fail_silently=False)
     return render(request, 'index.html', {'posts': posts, 'coordinators': coordinators})
